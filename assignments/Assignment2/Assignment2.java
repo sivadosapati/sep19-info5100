8d292ffad2be24b4c87eb39782086adfbae65566
Divya <divyasharma0792@gmail.com>
2019-09-15 23:01:45 -0700

    Updated

---
+++ assignments/Assignment2/Assignment2.java
@@ -0,0 +1,50 @@
+package Assignment2;
+
+public class Assignment2 {
+    /*
+        This method should return the sum of salaries of employees having salary greater than 5000
+        Note: Employee array is passed, not employee
+     */
+    public double salaryGreaterThanFiveThousand(Employee[] employees) {
+        // @TODO
+        return 0.0;
+    }
+
+    /*
+        This method should print either "Fizz", "Buzz" or "FizzBuzz"
+        "Fizz" - if id of employee is divisible by 3
+        "Buzz" - if id of employee is divisible by 5
+        "FizzBuzz" - if id of employee is divisible by both 3 and 5
+     */
+    public void fizzBuzz(Employee employee) {
+        // @TODO
+    }
+
+    /*
+        This method should calculate tax for an employee in dollars
+        If salary of employee is less than or equal to 2500, tax should be 10%
+        If salary of employee is greater than 2500 but less than or equal to 5000, tax should be 25%
+        If salary of employee is greater than 5000, tax should be 35%
+    */
+    public double calculateTax(Employee employee) {
+        // @TODO
+        return 0.0;
+    }
+
+    /*
+        We are given two employee objects.
+        Implement this method to swap salaries of employees
+    */
+    public void swap(Employee firstEmployee, Employee secondEmployee) {
+        // @TODO
+    }
+
+    /*
+        Return number of employees whose age is greater than 50
+        Note: Employee array is passed, not employee
+     */
+    public int employeesAgeGreaterThan50(Employee[] employees) {
+        // @TODO
+        return 0;
+    }
+}

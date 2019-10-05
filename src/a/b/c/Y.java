623a5a65f4e83a000e20a4e76f6995232aed99c7
Siva Dosapati <sdosapati@indeed.com>
2019-10-04 19:38:50 -0700

    Commit code and notes from class5

---
+++ src/a/b/c/Y.java
@@ -0,0 +1,11 @@
+package a.b.c;
+
+public class Y {
+	X x = new X();
+	Y(){
+		//int a = x.a;
+		int b = x.b;
+		int c = x.c;
+		int d = x.d;
+	}
+}
---
+++ src/facebook/posts/PostMessage.java
@@ -0,0 +1,5 @@
+package facebook.posts;
+
+public class PostMessage {
+
+}
